5730cdb708f2427fe05c005f8c0871c50b6d45b4
Charif Abdallah <charifabdallah@iut-dhaka.edu>
2021-09-02 21:11:29 +0600

    update

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     $(window).scroll(function() {
 
-        if (this.scrollY > 70) {
+        if (this.scrollY > 30) {
             $('.navbar').addClass("sticky");
         } else {
             $('.navbar').removeClass("sticky");
